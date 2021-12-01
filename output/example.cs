fd93ccc481f2182cff2d0e184da6771e85b6adae
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-01 16:48:23 +0100

    Handle parentheses.

--- output/example.cs
+++ output/example.cs
@@ -3,6 +3,6 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		2+2*5/3-5*6
+		(2+3)*5+4*6+((2*3)+(5-6)*4)
 	}
 }
--- src/visitor/TermVisitor.cs
+++ src/visitor/TermVisitor.cs
@@ -31,7 +31,23 @@ public class TermVisitor : Python3ParserBaseVisitor<Term>
     public override Term VisitAtom_expr([NotNull] Python3Parser.Atom_exprContext context)
     {
         result = new Term();
-        result.tokens.Add(context.atom().NUMBER().ToString());
+        if (context.atom().ChildCount == 1)
+        {
+            result.tokens.Add(context.atom().NUMBER().ToString());
+        } else if (context.atom().ChildCount == 3 &&
+            context.atom().GetChild(0).ToString() == "(" &&
+            context.atom().GetChild(2).ToString() == ")")
+        {
+            result.tokens.Add("(");
+            ArithExprVisitor internalVisitor = new ArithExprVisitor();
+            context.Accept(internalVisitor);
+            for (int i = 0; i < internalVisitor.result.tokens.Count; ++i)
+            {
+                result.tokens.Add(internalVisitor.result.tokens[i]);
+            }
+            result.tokens.Add(")");
+        }
+        
         return result;
     }
 }
