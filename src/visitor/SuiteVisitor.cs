4d62e3e5e72021a9bd24fca3c66a59f2a6936b50
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-09 20:02:43 +0100

    Add SuiteVisitor.

---
+++ src/visitor/SuiteVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Antlr4.Runtime.Misc;
+
+// This is a visitor to be used to compute a suite of statements
+public class SuiteVisitor : Python3ParserBaseVisitor<Suite>
+{
+    public Suite result;
+    public override Suite VisitSuite([NotNull] Python3Parser.SuiteContext context)
+    {
+        result = new Suite();
+        for (int i = 0; i < context.ChildCount; ++i)
+        {   
+            if (context.GetChild(i).ToString() != "\t" &&
+                context.GetChild(i).ToString() != "\n") {
+                // This means that the i-th child is a stmt node
+                StmtVisitor newVisitor = new StmtVisitor();
+                context.GetChild(i).Accept(newVisitor);
+                result.lines.Add(newVisitor.result.ToString());
+            }
+
+        }
+        return result;
+    }
+}
