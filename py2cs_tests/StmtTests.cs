9d29fef34b7f38a59bd9aaa26814214450a717de
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-13 16:04:40 +0100

    Add unit test.

--- py2cs_tests/StmtTests.cs
+++ py2cs_tests/StmtTests.cs
@@ -58,5 +58,44 @@ class Program
             Assert.Equal(expected, actual);
 
         }
+        [Fact]
+        public void Test2_NestedForLoop()
+        {
+            // Arrange
+            string input =
+@"a = [3, 4, 5]
+b = [1, 2]
+for x in a:
+	for y in b:
+		print(x + y)
+";
+            string expected =
+@"using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        dynamic a = new List<object> {3, 4, 5};
+        dynamic b = new List<object> {1, 2};
+        foreach (dynamic x in a)
+        {
+            foreach (dynamic y in b)
+            {
+                Console.WriteLine(x+y);
+            }
+        }
+    }
+}
+";
+            Translator translator = new Translator();
+            // Act
+            string actual = translator.Translate(input);
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+
     }
 }
