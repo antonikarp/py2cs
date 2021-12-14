b8e37a984a43116cc1a6e169f70980b6f8a786fb
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-14 22:32:52 +0100

    Add subscription in TrailerVisitor.

--- src/visitor/expressions/TrailerVisitor.cs
+++ src/visitor/expressions/TrailerVisitor.cs
@@ -1,6 +1,8 @@
 ﻿using Antlr4.Runtime.Misc;
 
-// This visitor is used in translating function calls.
+// This visitor is used in translating:
+// * function calls
+// * subscriptions (ex. dict_a["key"])
 public class TrailerVisitor : Python3ParserBaseVisitor<LineModel>
 {
     public LineModel result;
@@ -9,13 +11,16 @@ public class TrailerVisitor : Python3ParserBaseVisitor<LineModel>
     {
         state = _state;
     }
-    // Right now only 1 argument is handled
+    
     public override LineModel VisitTrailer([NotNull] Python3Parser.TrailerContext context)
     {
         result = new LineModel();
-        if (context.ChildCount == 3)
+        // Function call
+        if (context.ChildCount == 3 && context.GetChild(0).ToString() == "(" &&
+            context.GetChild(2).ToString() == ")")
         {
             result.tokens.Add("(");
+            // Right now only 1 argument is handled.
             OrTestVisitor newVisitor = new OrTestVisitor(state);
             context.GetChild(1).Accept(newVisitor);
             for (int i = 0; i < newVisitor.result.tokens.Count; ++i)
@@ -24,6 +29,20 @@ public class TrailerVisitor : Python3ParserBaseVisitor<LineModel>
             }
             result.tokens.Add(")");
             result.tokens.Add(";");
+        }
+        // Subscription
+        else if (context.ChildCount == 3 && context.GetChild(0).ToString() == "[" &&
+            context.GetChild(2).ToString() == "]")
+        {
+            result.tokens.Add("[");
+            // Right now only 1 argument is handled.
+            OrTestVisitor newVisitor = new OrTestVisitor(state);
+            context.GetChild(1).Accept(newVisitor);
+            for (int i = 0; i < newVisitor.result.tokens.Count; ++i)
+            {
+                result.tokens.Add(newVisitor.result.tokens[i]);
+            }
+            result.tokens.Add("]");
         }
         return result;
     }
