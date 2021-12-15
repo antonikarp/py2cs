c2f2ddcf172c885b15c63c20c93cb2f6d3835f33
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-15 12:16:38 +0100

    Add unit tests.

--- py2cs_tests/FuncTests.cs
+++ py2cs_tests/FuncTests.cs
@@ -1,1 +1,61 @@
-﻿
+﻿using System;
+using Xunit;
+using py2cs;
+
+namespace py2cs_tests
+{
+    public class FuncTests
+    {
+        [Fact]
+        public void Test1_VoidFunction()
+        {
+            // Arrange
+            string input =
+@"def foo():
+	print(""Hello"")
+";
+            string expected =
+@"using System;
+class Program
+{
+    public void foo()
+    {
+        Console.WriteLine(""Hello"");
+    }
+}
+";
+            Translator translator = new Translator();
+            // Act
+            string actual = translator.Translate(input);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test2_NonVoidFunction()
+        {
+            // Arrange
+            string input =
+@"def foo():
+	a = 2
+	return a
+";
+            string expected =
+@"using System;
+class Program
+{
+    public object foo()
+    {
+        dynamic a = 2;
+        return a;
+    }
+}
+";
+            Translator translator = new Translator();
+            // Act
+            string actual = translator.Translate(input);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
