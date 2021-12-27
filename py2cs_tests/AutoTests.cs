0b25094b9fc830be1934ca9b651249ef917611d9
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-27 12:37:02 +0100

    Add AutoTests.

---
+++ py2cs_tests/AutoTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using py2cs;
+
+public class AutoTests
+{
+    [Fact]
+    public void Test1_HelloWorld()
+    {
+        string input_path = "../../../../tests/scripts/test1.py";
+        string output_path = "../../../../tests/generated/test1.cs";
+        Test test = new Test();
+        test.RunTest(input_path, output_path);
+    }
+}
