deb14fa72dc2f9f5ef136551cb65eff1a72ccbf5
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-27 14:34:55 +0100

    Add a single test.

---
+++ tests/run_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+names=(test1)
+
+for name in "${names[@]}"
+do
+	# Run the test script and get its output.
+	python3 scripts/"$name".py > scripts_output/"$name".txt
+
+	# Perform translation.
+	cd ..
+	dotnet test > /dev/null 2>&1
+	cd tests
+
+	# Compile the obtained .cs file.
+	mcs generated/"$name".cs
+
+	# Run the compiled program and save its output.
+	mono generated/"$name".exe > generated_output/"$name".txt
+
+	# Compare the two outputs. If they match, then the test passed.
+	python3 compare_results.py ./scripts_output/"$name".txt ./scripts_output/"$name".txt "$name"
+
+done
