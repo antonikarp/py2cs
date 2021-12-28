6e4e7ca2eb346da71291395930c62809a4143419
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-28 18:20:50 +0100

    Add handling of iteration over dict/set.

--- src/visitor/statements/ExprStmtVisitor.cs
+++ src/visitor/statements/ExprStmtVisitor.cs
@@ -17,7 +17,6 @@ public class ExprStmtVisitor : Python3ParserBaseVisitor<LineModel>
         // Todo: handle assignment.
         if (context.ChildCount == 3 && context.GetChild(1).ToString() == "=")
         {
-            state.varState = new VarState();
             TestVisitor leftVisitor = new TestVisitor(state);
             TestVisitor rightVisitor = new TestVisitor(state);
             context.GetChild(0).Accept(leftVisitor);
--- tests/scripts/test9.py
+++ tests/scripts/test9.py
@@ -1,13 +1,21 @@
 # Sets and dicts.
 
+# Due to unspecified ordered of iteration over keys, the value which is printed
+# is constant.
 new_set = {"a", "b", "c"}
 if "a" in new_set:
 	print("Yes")
 else:
 	print("No")
 
+for x in new_set:
+	print("set")
+
 new_dict = {"d": 1, "e": 2, "f": 3}
 if "h" in new_dict:
 	print("Yes")
 else:
 	print("No")
+
+for y in new_dict:
+	print("dict")
