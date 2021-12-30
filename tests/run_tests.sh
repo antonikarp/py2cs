2570423d998fe4f4806cd122815493e345ca413d
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2021-12-30 10:24:40 +0100

    Handle command line arguments.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-names=(test1 test2 test3 test4 test5 test6 test7 test8 test9 test10 test11 test12)
+names=(test1 test2 test3 test4 test5 test6 test7 test8 test9 test10 test11)
 
 # First delete the previous generated files.
 bash ./clean_dirs.sh
