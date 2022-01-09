0eccbe184d739e6ca1d79e94f47a0fc2956e538a
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2022-01-09 12:47:49 +0100

    Handle set comprehension.

--- tests/scripts/temp1.py
+++ tests/scripts/temp1.py
@@ -1,3 +1,5 @@
-print( [x for x in enumerate([2,3,5,7,11])])
-for x in enumerate([2,3,5,7,11]):
-    print(x)
+print("_TEST_UNORDERED_ON")
+print( { i**2 for i in range(10) if i%2==0 } )
+print( { x for x in "Test" } )
+print("_TEST_UNORDERED_OFF")
+print( [x for x in [1, 2, 3, 4]])
