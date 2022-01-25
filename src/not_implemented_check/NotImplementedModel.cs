ea15a34f262b4529ac4ef69971d9ba3d0ae49789
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2022-01-25 18:07:31 +0100

    Add some not implemented features to NotImplementedCheckVisitor.

--- src/not_implemented_check/NotImplementedModel.cs
+++ src/not_implemented_check/NotImplementedModel.cs
@@ -2,8 +2,10 @@
 public class NotImplementedModel
 {
     public HashSet<string> declaredFunctions;
+    public HashSet<string> declaredClasses;
     public NotImplementedModel()
     {
         declaredFunctions = new HashSet<string>();
+        declaredClasses = new HashSet<string>();
     }
 }
