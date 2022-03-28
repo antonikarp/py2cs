838bce93812580ab67d3b8b9346855b1d70b8bfb
Antoni Karpiński <antoni.karpinski@Antonis-MacBook-Pro.local>
2022-03-28 16:53:10 +0200

    Update tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,33 +9,33 @@ dotnet test
 cd tests
 
 # 1a. -------------- Tests in which the results are the same. Import mechanism.
-dir_names_1=(1 2 3 4)
+dir_names_1=(1 2 3 4 5)
 
 for dir_name in "${dir_names_1[@]}"
 do
-	cat scripts/1_must_have/import/"$dir_name"/testnames.txt 2> /dev/null | while read name
+	cat scripts/1_correct/import/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do
 		# Run the test script and get its output.
 	
-		python3 scripts/1_must_have/import/"$dir_name"/"$name".py > scripts_output/1_must_have/import/"$dir_name"/"$name".txt
+		python3 scripts/1_correct/import/"$dir_name"/"$name".py > scripts_output/1_correct/import/"$dir_name"/"$name".txt
 		
 		cd ..
-		dotnet run tests/scripts/1_must_have/import/"$dir_name"/"$name".py tests/generated/1_must_have/import/"$dir_name"
+		dotnet run tests/scripts/1_correct/import/"$dir_name"/"$name".py tests/generated/1_correct/import/"$dir_name"
 		cd tests
 		
 		echo
 		
-		csc generated/1_must_have/import/"$dir_name"/*.cs /out:generated/1_must_have/import/"$dir_name"/"$name".exe
+		csc generated/1_correct/import/"$dir_name"/*.cs /out:generated/1_correct/import/"$dir_name"/"$name".exe
 
 		# Run the compiled program and save its output.
-		mono generated/1_must_have/import/"$dir_name"/"$name".exe > generated_output/1_must_have/import/"$dir_name"/"$name".txt
+		mono generated/1_correct/import/"$dir_name"/"$name".exe > generated_output/1_correct/import/"$dir_name"/"$name".txt
 		
 		echo
 	done	
 done
 
 # 1b. -------------- Tests in which the result are the same. Take data to stdin from external files (.in)
-dir_names_2=(1_must_have/input)
+dir_names_2=(1_correct/input)
 
 for dir_name in "${dir_names_2[@]}"
 do
@@ -60,7 +60,7 @@ do
 done
 
 # 1c. -------------- Tests in which the script and the translated program give the same results.
-dir_names_3=(1_must_have 2_should_have)
+dir_names_3=(1_correct)
 
 for dir_name in "${dir_names_3[@]}"
 do
@@ -84,35 +84,9 @@ do
 	done	
 done
 
-
-
-# 2. -------------- Tests which probably won't pass, but need to fail gracefully (with an error message).
-dir_names_4=(3_nice_to_have)
+# 2. -------------- Tests which check scripts with language constructs which are not handled.
+dir_names_4=(2_not_implemented)
 for dir_name in "${dir_names_4[@]}"
-do
-	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
-	do
-		# Run the test script and get its output.
-		python3 -O scripts/"$dir_name"/"$name".py > scripts_output/"$dir_name"/"$name".txt
-		
-		cd ..
-		dotnet run tests/scripts/"$dir_name"/"$name".py tests/generated/"$dir_name"
-		cd tests
-		
-		echo
-		
-		csc generated/"$dir_name"/"$name"*.cs /out:generated/"$dir_name"/"$name".exe /langversion:8.0
-		
-		#If the compiled program exists, run it and save its output.
-		mono generated/"$dir_name"/"$name".exe > generated_output/"$dir_name"/"$name".txt
-		
-		echo
-	done
-done
-
-# 3. -------------- Tests which check scripts with language constructs which are not handled.
-dir_names_5=(4_not_implemented)
-for dir_name in "${dir_names_5[@]}"
 do
 	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do
@@ -125,11 +99,11 @@ do
 	done
 done
 
-# 4. -------------- Tests of programs which generate an error. The result of translation is a message with an error.
+# 3. -------------- Tests of programs which generate an error. The result of translation is a message with an error.
 
-dir_names_6=(5_error)
+dir_names_5=(3_incorrect_scripts)
 
-for dir_name in "${dir_names_6[@]}"
+for dir_name in "${dir_names_5[@]}"
 do
 	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do
@@ -141,10 +115,10 @@ do
 	done
 done
 
-# 5. -------------- Tests which give different results in Python and C#.
-dir_names_7=(6_differences)
+# 4. -------------- Tests which give different results in Python and C#.
+dir_names_6=(4_differences)
 
-for dir_name in "${dir_names_7[@]}"
+for dir_name in "${dir_names_6[@]}"
 do
 	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do
@@ -172,10 +146,10 @@ echo "----------RESULTS----------"
 
 for dir_name in "${dir_names_1[@]}"
 do
-	cat scripts/1_must_have/import/"$dir_name"/testnames.txt 2> /dev/null | while read name
+	cat scripts/1_correct/import/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do
 		# Compare the two outputs. If they match, then the test passed.
-		python3 compare_results_identical.py ./generated_output/1_must_have/import/"$dir_name"/"$name".txt ./scripts_output/1_must_have/import/"$dir_name"/"$name".txt "1_must_have/import/${dir_name}"
+		python3 compare_results_identical.py ./generated_output/1_correct/import/"$dir_name"/"$name".txt ./scripts_output/1_correct/import/"$dir_name"/"$name".txt "1_correct/import/${dir_name}"
 	done
 done
 
@@ -206,8 +180,8 @@ for dir_name in "${dir_names_4[@]}"
 do
 	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do
-		# Compare the two outputs. If they match, then the test passed.
-		python3 compare_or_validate_nice_to_have.py ./generated_output/"$dir_name"/"$name".txt ./scripts_output/"$dir_name"/"$name".txt ./generated/"$dir_name"/"$name".txt "${dir_name}/${name}"
+		# Instead of .cs file there is a generated .txt file
+		python3 validate_results_not_implemented.py ./generated/"$dir_name"/"$name".txt "${dir_name}/${name}"
 	done
 	echo
 done
@@ -217,22 +191,12 @@ do
 	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do
 		# Instead of .cs file there is a generated .txt file
-		python3 validate_results_not_implemented.py ./generated/"$dir_name"/"$name".txt "${dir_name}/${name}"
-	done
-	echo
-done
-
-for dir_name in "${dir_names_6[@]}"
-do
-	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
-	do
-		# Instead of .cs file there is a generated .txt file
 		python3 validate_results_error.py ./generated/"$dir_name"/"$name".txt "${dir_name}/${name}"
 	done
 	echo
 done
 
-for dir_name in "${dir_names_7[@]}"
+for dir_name in "${dir_names_6[@]}"
 do
 	cat scripts/"$dir_name"/testnames.txt 2> /dev/null | while read name
 	do	
